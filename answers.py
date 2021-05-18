8bbd28e410feff7d97a93c5ec6cdaaf5cb228073
Evgeny Surogin <sacccp@gmail.com>
2021-05-18 08:39:13 +0300

    Deleted some code

--- answers.py
+++ answers.py
@@ -6,12 +6,3 @@ while n != 0:
 lst.sort()
 m = max(lst)
 print(lst.count(m))
-
-a, b = int(input()), int(input())
-while a != b:
-    if (a // 2 >= b) and (a % 2 == 0):
-        print(':2')
-        a //= 2
-    else:
-        print('-1')
-        a -= 1
